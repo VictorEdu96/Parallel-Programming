48acf51b5de5ce9566c7f6da455fa015d08ab1f8
Victor Pulido <victoredu96@hotmail.com>
2019-02-05 19:17:23 -0600

    Revert changes from previous example and revampt from the beginning.

--- Matrix Transposition Version 1/transposition.c
+++ Matrix Transposition Version 1/transposition.c
@@ -1,47 +1,44 @@
 #include <stdio.h>
 #include <time.h>
+#define N 5
+#define M 3
 
-//Funcion para solo imprimir matriz.
-// void showMatrix(double *a, int w, int l) {
-//   for (int i = 0; i < w; i++) {
-//     for (int j = 0; j < l; j++) {
-//       printf("%g%c", a[i][j], j == 2 ? '\n' : ' ');
-//     }
-//     puts("\n");
-//   }
-// }
-
-//Transponer la matriz
-void transposition(void *original, void *destino, int w, int l) {
-
-double (*d)[w] = original;
-double (*s)[l] = destino;
-  for (int i = 0; i < w; i++) {
-    for (int j = 0; j < l; j++) {
-      d[j][i] = s[i][j];
+void showMatrix(int matrix[N][M]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      printf("%d\t", matrix[i][j]);
     }
+    printf("\n");
   }
-  // showMatrix(*d,3,3);
 }
 
 int main(int argc, char const *argv[]) {
-  int i, j;
   clock_t t;
-  double threeByThree[10][10] = {{1,2,3,5,3,6,7,5,4,3},
-                                {4,5,6,6,4,7,8,6,3,6},
-                                {7,8,9,6,1,4,5,8,3,9},
-                                {7,5,3,7,9,0,5,2,7,8},
-                                {6,2,3,3,4,5,7,8,9,10},
-                                {1,2,3,5,3,6,7,5,4,3},
-                                {4,5,6,6,4,7,8,6,3,6},
-                                {7,8,9,6,1,4,5,8,3,9},
-                                {7,5,3,7,9,0,5,2,7,8},
-                                {6,2,3,3,4,5,7,8,9,10}};
+  int matrix[N][M];
+  int transpose[N][M];
+  int i, j;
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      matrix[i][j] = rand() % N;
+    }
+  }
 
-  double transposedMatrix[10][10];
-  transposition(transposedMatrix, threeByThree, 10, 10);
-  for (i = 0; i < 10; i++)
-		for (j = 0; j < 10; j++)
-			printf("%g%c", transposedMatrix[i][j], j == 9 ? '\n' : ' ');
-  return 0;
+  showMatrix(matrix);
+  t = clock();
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      transpose[i][j] = matrix[j][i];
+    }
+    printf("\n");
+  }
+
+  t = clock() - t;
+  double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+
+  printf("Tranposing) took %f seconds to execute \n", time_taken);
+
+
+  showMatrix(transpose);
+
 }
